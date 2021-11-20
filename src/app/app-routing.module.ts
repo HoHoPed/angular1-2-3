fb99cacba1c9ed4c181de9112014451a629bef92
HoHoPed <gabriel.holanda4@hotmail.com>
2021-11-19 21:41:25 -0300

    upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavComponent } from './body/nav/nav.component';
+import { TaskFormComponent } from './tasks/task-form/task-form.component';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
+import { HeaderComponent } from './views/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:'',
+  component:NavComponent
+},
+{
+  path:'sobre',
+  component:TaskListComponent
+},
+{
+  path:'contato',
+  component:TaskFormComponent
+}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './views/header/header.component';
+import { NavComponent } from './body/nav/nav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
+import { TaskListItemComponent } from './tasks/task-list-item/task-list-item.component';
+import { TaskFormComponent } from './tasks/task-form/task-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    TaskListComponent,
+    TaskListItemComponent,
+    TaskFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
